01f849569f6d62396e6605cb2d548ca55113940a
Paul Klink <paul@klink.au>
2024-12-13 14:11:39 +1100

    Improve redirect for csharp-basic-example-meta-file

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,26 +11,26 @@ export default defineConfig({
   trailingSlash: 'always',
 
   redirects: {
-      '/Music/index.html': '/music/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Music/index.html': '/music/', // Will not compile went built on Windows due to case insensitive file match
       '/Music/SailorJim.html': '/music/sailor-jim/',
-      '/Music/montage/index.html': '/music/montage/', // Will not compile went built on Windows due to case insensitive file match
-      '/Music/montage/bio/index.html': '/music/montage/bio/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Music/montage/index.html': '/music/montage/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Music/montage/bio/index.html': '/music/montage/bio/', // Will not compile went built on Windows due to case insensitive file match
       '/Music/montage/songs.html': '/music/montage/songs/',
-      '/Software/index.html': '/software/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Software/index.html': '/software/', // Will not compile went built on Windows due to case insensitive file match
       '/Software/FieldedText/index.html': '/software/fielded-text/',
-      '/Software/Delphi/index.html': 'software/delphi/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Software/Delphi/index.html': 'software/delphi/', // Will not compile went built on Windows due to case insensitive file match
       '/Software/Delphi/ActionUpdateInActiveX.htm': '/software/delphi/action-update-in-active-x/',
       '/Software/Delphi/DebuggingIsapiWithIisExpress.htm': '/software/delphi/debugging-isapi-with-iis-express/',
       '/Software/Delphi/DebuggingIsapiWithIisExpress': '/software/delphi/debugging-isapi-with-iis-express/',
       '/Software/Delphi/ImprovedRadioGroup.htm': '/software/delphi/improved-radio-group/',
-      '/Software/Retab/index.html': '/software/retab/', // Will not compile went built on Windows due to case insensitive file match
+      // '/Software/Retab/index.html': '/software/retab/', // Will not compile went built on Windows due to case insensitive file match
       '/Software/Retab/Retab.pdf': '/software/retab/retab.pdf',
-      '/software/fielded-text/c-sharp-library/examples/index.html': '/software/fielded-text/c-sharp-library/examples/',
-      '/c-sharp-library/examples/write_headings.html': '/c-sharp-library/examples/csharp-writing-of-csv-file-including-headings/',
-      '/c-sharp-library/examples/write_events.html': '/c-sharp-library/examples/csharp-writing-of-csv-file-using-events/',
-      '/c-sharp-library/examples/write_comments.html': '/c-sharp-library/examples/csharp-writing-of-csv-file-with-comments/',
-      '/delphi-library/examples/basicread.html': '/delphi-library/examples/delphi-basic-reading-of-csv-file/',
-      '/delphi-library/examples/basicwrite.html': '/delphi-library/examples/delphi-basic-writing-of-csv-file/',
+      '/c-sharp-library/examples/write_headings.html': '/software/fielded-text/c-sharp-library/examples/csharp-writing-of-csv-file-including-headings/',
+      '/c-sharp-library/examples/write_events.html': '/software/fielded-text/c-sharp-library/examples/csharp-writing-of-csv-file-using-events/',
+      '/c-sharp-library/examples/write_comments.html': '/software/fielded-text/c-sharp-library/examples/csharp-writing-of-csv-file-with-comments/',
+      '/software/fielded-text/c-sharp-library/examples/csharp-basic-example-meta-file/': '/software/fielded-text/c-sharp-library/examples/csharp-basic-example-meta-file',
+      '/delphi-library/examples/basicread.html': '/software/fielded-text/delphi-library/examples/delphi-basic-reading-of-csv-file/',
+      '/delphi-library/examples/basicwrite.html': '/software/fielded-text/delphi-library/examples/delphi-basic-writing-of-csv-file/',
       '/post/a-variation-of-cost-disease/': '/articles/a-variation-of-cost-disease/',
       '/post/closing-the-r+d-funding-circle/': '/articles/closing-the-rd-funding-circle/',
       '/post/innovation-diffusion/': '/articles/innovation-diffusion/',
